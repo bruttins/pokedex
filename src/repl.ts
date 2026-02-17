36c38336276097d899ac4ed71d34631f609c3834
bruttins <bruttins@gmail.com>
2026-02-17 12:39:50 +0100

    added basic repl-functionality

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,6 @@
+import { createInterface } from "readline";
+import { getCommands } from "./commands.js";
+
 export function cleanInput(input: string): string[] {
   const result = [];
   const splitArray = input.split(" ");
@@ -6,4 +9,34 @@ export function cleanInput(input: string): string[] {
     if (cleaned !== "") result.push(cleaned);
   }
   return result;
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,  
+    output: process.stdout,
+    prompt: 'Pokedex > '
+  });
+  
+  rl.prompt(true);
+  rl.on('line', (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt(true);
+      return;
+    }
+    const commands = getCommands();
+    const commandName = words[0];
+    const cmd = commands[commandName];
+    if (cmd) {
+      try {
+        cmd.callback(commands);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log("Unknown command");
+    }
+    rl.prompt(true);
+  });
 }
